35b4db1634ff128ae652481a92e8cc6e422bd284
c5apple <c5apple_party@yahoo.co.jp>
2017-08-14 17:26:23 +0900

    #5 ウェビナーサービス作成

---
+++ src/app/shared/service/webinar/webinar-mock.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+import { WebinarService } from './webinar.service';
+
+@Injectable()
+export class WebinarMockService extends WebinarService {
+
+}
--- src/app/webinar/webinar-list/webinar-list.module.ts
+++ src/app/webinar/webinar-list/webinar-list.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { WebinarListComponent } from './webinar-list.component';
+import { WebinarService, WebinarMockService } from 'shared/service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [WebinarListComponent]
+  declarations: [WebinarListComponent],
+  providers: [
+    { provide: WebinarService, useClass: WebinarMockService }
+  ]
 })
 export class WebinarListModule { }
