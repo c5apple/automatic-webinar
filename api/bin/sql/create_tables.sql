e86b4437b08f0ee7aa109e5e9ca3f325e9b39312
c5apple <c5apple_party@yahoo.co.jp>
2017-08-14 12:28:37 +0900

    #14 データベース作成

---
+++ api/bin/sql/create_tables.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'アカウントID',
+  `login_id` varchar(32) NOT NULL COMMENT 'アカウントID',
+  `pass` varchar(255) NOT NULL COMMENT 'パスワード',
+  `name` varchar(32) COMMENT 'アカウント名',
+  `delete_flg` tinyint(1) DEFAULT '0' COMMENT '削除フラグ',
+  `create_day` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '作成日付',
+  `create_user` int(11) NOT NULL COMMENT '作成ユーザ',
+  `update_day` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '更新日付',
+  `update_user` int(11) NOT NULL COMMENT '更新ユーザ',
+  PRIMARY KEY (`id`),
+  KEY `login_id` (`login_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='アカウント'
+;
+
+DROP TABLE IF EXISTS `webinar`;
+CREATE TABLE `webinar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ウェビナーID',
+  `name` varchar(60) NOT NULL COMMENT 'ウェビナー名',
+  `delete_flg` tinyint(1) DEFAULT '0' COMMENT '削除フラグ',
+  `create_day` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '作成日付',
+  `create_user` int(11) NOT NULL COMMENT '作成ユーザ',
+  `update_day` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '更新日付',
+  `update_user` int(11) NOT NULL COMMENT '更新ユーザ',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ウェビナー'
+;
+
+DROP TABLE IF EXISTS `opt`;
+CREATE TABLE `opt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `webinar_id` int(11) NOT NULL COMMENT 'ウェビナーID',
+  `mail` varchar(254) NOT NULL COMMENT 'メールアドレス',
+  `preferred_date` date NOT NULL COMMENT '希望日',
+  `delete_flg` tinyint(1) DEFAULT '0' COMMENT '削除フラグ',
+  `create_day` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '作成日付',
+  `create_user` int(11) NOT NULL COMMENT '作成ユーザ',
+  `update_day` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '更新日付',
+  `update_user` int(11) NOT NULL COMMENT '更新ユーザ',
+  PRIMARY KEY (`id`),
+  KEY `webinar_id` (`webinar_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='オプト'
+;
