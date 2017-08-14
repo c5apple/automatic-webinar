e992d62a953ff529d70491b598c190b2a22daf4e
c5apple <c5apple_party@yahoo.co.jp>
2017-08-14 18:37:27 +0900

    #5 Prodモード

--- src/app/shared/service/webinar/webinar-mock.service.ts
+++ src/app/shared/service/webinar/webinar-mock.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { WebinarService } from './webinar.service';
 
+/**
+ * ウェビナーサービス
+ * モック
+ */
 @Injectable()
 export class WebinarMockService extends WebinarService {
+
+  /**
+   * ウェビナーを取得する
+   * @param webinarId ウェビナーID
+   */
+  public getWebinar(webinarId: number): Observable<Object> {
+    return Observable.of({});
+  }
 
 }
--- src/app/webinar/webinar-list/webinar-list.module.ts
+++ src/app/webinar/webinar-list/webinar-list.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { environment } from 'env/environment';
 import { WebinarListComponent } from './webinar-list.component';
-import { WebinarService, WebinarMockService } from 'shared/service';
 
 @NgModule({
   imports: [
@@ -10,7 +10,7 @@ import { WebinarService, WebinarMockService } from 'shared/service';
   ],
   declarations: [WebinarListComponent],
   providers: [
-    { provide: WebinarService, useClass: WebinarMockService }
+    environment.webinarService
   ]
 })
 export class WebinarListModule { }
