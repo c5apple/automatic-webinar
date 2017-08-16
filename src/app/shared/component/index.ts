91733b9e4103887630c477c48bd911ef93a398b5
c5apple <c5apple_party@yahoo.co.jp>
2017-08-16 17:50:56 +0900

    #27 フッター

--- src/app/shared/component/index.ts
+++ src/app/shared/component/index.ts
@@ -1,3 +1,4 @@
 export * from './confirm-dialog/confirm-dialog.component';
 export * from './confirm-dialog/confirm-dialog.module';
+export * from './footer/footer.module';
 export * from './header/header.module';
