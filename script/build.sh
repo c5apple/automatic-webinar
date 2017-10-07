14c2078241a2d0750474d334e43beca0b3dfe96b
c5apple <c5apple_party@yahoo.co.jp>
2017-10-08 02:15:17 +0900

    #52 難読化スクリプト

---
+++ script/build.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+rm -rf dist/api
+mkdir -p dist/api
+FILES=`find ./api -type f -name "*.php"`
+for FILE in ${FILES}
+do
+  # Obfuscate!!
+  STRIPPED=`php -w ${FILE} | sed -e "s/<?php//" | sed -e "s/'/\"/g"`
+  ENCODED=`php -r "echo base64_encode('${STRIPPED}');"`
+  OBFUSCATED=`echo "<?php @eval(base64_decode('${ENCODED}'));"`
+
+  # Output!!
+  DIRNAME=`dirname ${FILE}`
+  FILENAME=`basename ${FILE}`
+  mkdir -p dist/${DIRNAME}
+  echo ${OBFUSCATED} > dist/${DIRNAME}/${FILENAME}
+done
