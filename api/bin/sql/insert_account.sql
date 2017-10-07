79fe4c9f45b734539630b8a75be28791e7887ab5
c5apple <c5apple_party@yahoo.co.jp>
2017-10-08 06:06:31 +0900

    テストユーザ

---
+++ api/bin/sql/insert_account.sql
@@ -0,0 +1,5 @@
+INSERT INTO `account`
+(`id`,`login_id`,`pass`,`name`,`delete_flg`,`create_day`,`create_user`,`update_day`,`update_user`)
+VALUES
+(NULL,'test','$2y$10$tAacOvOjQqzYmjDqt4KSfeW4t.pbPDgDQmOgS4thVDp0Wgnyl4zKG','テストユーザ',0,NOW(),0,NOW(),0)
+;
