7cfdfdafb499073d93be5739c74ffa4dec1e1d0d
c5apple <c5apple_party@yahoo.co.jp>
2018-02-06 13:49:23 +0900

    LP 複数フォーム対応

--- lp/js/automatic-webinar-1.3.0.js
+++ lp/js/automatic-webinar-1.3.0.js
@@ -1,32 +1,35 @@
-/* AutomaticWebinar(v1.2.3) */
+/* AutomaticWebinar(v1.3.0) */
 $(function () {
   "use strict";
-  var $form = $('#aw-form');
 
   var offset = 2;
   var now = new Date();
   now.setDate(now.getDate() + offset);
+  var opt = '';
   for (var i = 0; i < 3; i++) {
     now.setDate(now.getDate() + 1);
     var y = now.getFullYear() + '';
     var m = ('0' + (now.getMonth() + 1)).slice(-2);
     var d = ('0' + now.getDate()).slice(-2);
     var w = ["日", "月", "火", "水", "木", "金", "土"][now.getDay()];
-    $form.find('#preferredDate').append(
-      '<option value="' + y + m + d + '">' + y + '/' + m + '/' + d + '(' + w + ')</option>');
+    opt += '<option value="' + y + m + d + '">' + y + '/' + m + '/' + d + '(' + w + ')</option>';
   }
+  $('.preferredDate').each(function () {
+    $(this).html(opt);
+  });
 
-  $('#aw-subscribe').on('click', function (event) {
+  $('.aw-subscribe').on('click', function (event) {
     if (event) event.preventDefault();
+    var $form = $(this).closest('.aw-form');
     if ($form.valid()) {
       // opt登録
       $.post($form.attr('action'), $form.serialize()
       ).done(function (data) {
-        var successMessage = $('#aw-success').html();
-        $('#aw-notification').hide().html(successMessage).fadeIn("slow");
+        var successMessage = $('.aw-success').html();
+        $('.aw-notification').hide().html(successMessage).fadeIn("slow");
       }).fail(function (err) {
-        var errorMessage = $('#aw-error').html();
-        $('#aw-notification').hide().html(errorMessage).fadeIn("slow");
+        var errorMessage = $('.aw-error').html();
+        $('.aw-notification').hide().html(errorMessage).fadeIn("slow");
       });
     }
   });
